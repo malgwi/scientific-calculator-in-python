45915182a2ad1a8dd7a03b0c62bc3a226719be9c
-Sunday <malgwisunday@gmail.com>
2017-12-28 18:48:44 +0100

    first commit

---
+++ calculator.py
@@ -0,0 +1,42 @@
+# A simple calculator
+ 
+while True:
+    print('****OPTIONS****')
+    print("Enter '+' to add two numbers.")
+    print("Enter '-' to subtract two numbers.")
+    print("Enter '*' to multiply two numbers.")
+    print("Enter '/' to dividce two numbers.")
+    print("Else enter 'EXIT' to end program")
+    user_input=input("")
+ 
+    if user_input == "EXIT":
+        break
+    elif user_input == "+":
+        num1 = float(input('Enter first number'))
+        num2 = float(input('Enter the other number'))
+        result = str(num1+num2)
+        print('The answer is: '+result)
+        break
+    elif user_input == "-":
+        num1 = float(input('Enter first number'))
+        num2 = float(input('Enter the other number'))
+        result = str(num1-num2)
+        print('The answer is: '+result)
+        break
+    elif user_input == "*":
+        num1 = float(input('Enter first number'))
+        num2 = float(input('Enter the other number'))
+        result = str(num1*num2)
+        print('The answer is: '+result)
+        break
+    elif user_input == "/":
+        num1 = float(input('Enter first number'))
+        num2 = float(input('Enter the other number'))
+        result = str(num1/num2)
+        print('The answer is: '+result)
+        break
+    else:
+        print("Unknown input Silly")
+        break
+ 
+print("\n\n Powered by Santos")
